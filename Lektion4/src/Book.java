0d243f5b66c124b7f4398116dcbcc7f18d66cb10
Valdemar Segerström <valdemar.segerstrom.java25@edu.edugrade.se>
2025-09-22 19:05:43 +0200

    fortsattjobb

--- Lektion4/src/Book.java
+++ Lektion4/src/Book.java
@@ -1,7 +1,28 @@
 public class Book {
-    String titel;
+    String title;
     String author;
     int pages;
 
+    public Book() {
+    }
 
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+
+    }
+
+    void info() {
+        System.out.println("Book title: " + title + ", and author: " + author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", pages=" + pages +
+                '}';
+    }
 }
--- Lektion4/src/Main.java
+++ Lektion4/src/Main.java
@@ -5,13 +5,17 @@ public class Main {
 
         System.out.println("Hello and welcome!");
 
-        Book lotr = new Book();
-        lotr.author = "JRR Tolkien";
-        lotr.titel = "Lord of the Rings";
-        lotr.pages = 1200;
+//        Book lotr = new Book();
+//        lotr.author = "JRR Tolkien";
+//        lotr.title = "Lord of the Rings";
+//        lotr.pages = 1200;
 
-        System.out.println("Hur många sidor har Lotr: " + lotr.pages);
+        Book book1 = new Book("Lord of the rings", "JRR Tolkien", 1200);
 
+        System.out.println("Hur många sidor har Lotr: " + book1.pages);
 
+        System.out.println("Info om book1: " + book1.toString());
+
+     book1.info();
     }
 }

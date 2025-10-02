f34998bdbb9ef1377b64e61a502c7bbb2d85b0d4
Valdemar Segerström <valdemar.segerstrom.java25@edu.edugrade.se>
2025-10-02 20:12:58 +0200

    Uppgift 4 klar

--- lektion_4/src/Main.java
+++ lektion_4/src/Main.java
@@ -7,11 +7,21 @@ public class Main {
         // System.out.println(bookOne.toString());
         bookOne.info();
         Book bookTwo = new Book("Stora Boken om Lagar", "Svergies Regering", 1500, "Juridik");
-        Book bookThree = new Book ("Kart-Atlas", "Vägverket", 500, "Kartor");
-bookOne.readingTime();
-bookTwo.readingTime();
-bookThree.readingTime();
-bookOne.isItFantasy();
-bookTwo.isItFantasy();
+        Book bookThree = new Book("Kart-Atlas", "Vägverket", 500, "Kartor");
+        bookOne.readingTime();
+        bookTwo.readingTime();
+        bookThree.readingTime();
+        bookOne.isItFantasy();
+        bookTwo.isItFantasy();
+        compareBooks(bookOne.pages, bookTwo.pages);
+    }
+
+    static void compareBooks(double a, double b) {
+        if (a == b) {
+            System.out.println("Böckerna är lika långa!");
+        } else if (a > b) {
+            System.out.println("Den första boken är längre!");
+        } else System.out.println("Den andra boken är längre!");
+
     }
 }

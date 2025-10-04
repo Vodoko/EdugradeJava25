3c3df64bcd73b8b10f61cf9949889eeb29c12bf4
Valdemar Segerström <valdemar.segerstrom.java25@edu.edugrade.se>
2025-10-04 19:16:08 +0200

    Update Arrayers.java

--- Lektion_6/src/Arrayers.java
+++ Lektion_6/src/Arrayers.java
@@ -3,16 +3,33 @@ public class Arrayers {
         System.out.println(" --- Lektion 6 Arrays --- ");
         int[] numbers = new int[5];
         numbers[0] = 2;
-        numbers[1] = 4;
-        numbers[2] = 6;
+        numbers[1] = 3;
+        numbers[2] = 5;
         numbers[3] = 8;
-        numbers[4] = 10;
+        numbers[4] = 13;
 
-        int heltal = 0;
+//        int heltal = numbers[0];
+//
+//        for (int i = 1; i < numbers.length; i++) {
+//            heltal = heltal + numbers[i];
+//        System.out.println("Summan av arrayen numbers är: " + heltal);
+//        }
 
-        for (int i = 0; i < numbers.length; i++) {
-            heltal = heltal + numbers[i];
+//        int numberOfEvenNumbers = 0;
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 2 == 0) {
+//                numberOfEvenNumbers++;
+//            }
+//            System.out.println("Talet som checkas är: " + numbers[i]);
+//            System.out.println("Antal jämna tal är: " + numberOfEvenNumbers);
+//        }
+        int minstaTal = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < minstaTal) {
+                minstaTal = numbers[i];
+            }
         }
-        System.out.println("Summan av arrayen numbers är: " + heltal);
+        System.out.println("Minsta talet i arrayen är: " + minstaTal);
     }
 }

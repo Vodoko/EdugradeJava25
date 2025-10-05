1f384f1f09a506f908cf7fee3d050d45ae03608c
Valdemar Segerström <valdemar.segerstrom.java25@edu.edugrade.se>
2025-10-05 12:53:47 +0200

    fortsättningmetoderimain

--- Lektion_6/src/Main.java
+++ Lektion_6/src/Main.java
@@ -9,10 +9,25 @@ public class Main {
 
         Student student1 = new Student("Valdemar Segerström");
 
+//        example1();
+        example2();
+    }
+    private static void example1(){
+        Student student1 = new Student("Valdemar Segerström");
         Classroom java25 = new Classroom(5);
         java25.addStudent(student1);
-        java25.addStudent(new Student("Valdemar Eriksson"));
-
-
+        java25.addStudent(new Student("Farbror Eriksson"));
+        java25.addStudent(new Student("Mister Mountaineer"));
+        java25.addStudent(new Student("Miss Greece"));
+        java25.addStudent(new Student("Skelleftep Boi"));
+        java25.listStudents();
     }
+private static void example2() {
+    String[] names = {"Valdemar", "Eriksson", "Mountaineer", "Greece", "Skeleboi"};
+    Classroom java25 = new Classroom(5);
+    for (int i = 0; i < names.length; i++){
+        java25.addStudent(new Student(names[i]));
+    }
+    java25.listStudents();
+}
 }

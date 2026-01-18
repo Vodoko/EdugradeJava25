7956c9d93d92e7b1d4b4a222f51b19068f23993c
Valdemar Segerström <valdemar.segerstrom.java25@edu.edugrade.se>
2026-01-18 12:45:13 +0100

    Trying to get plugins to work

--- 4Design&Arkitektur/Lektion1/untitled/src/main/java/org/example/Main.java
+++ 4Design&Arkitektur/Lektion1/untitled/src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
+package
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        goodCalculator.setStrategy(new SameDayDelivery());
+        System.out.println("Same-day shipping cost: " + goodCalculator.calculate(weight) + " kr");
     }
 }

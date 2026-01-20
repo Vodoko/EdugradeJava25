e2e9634c03de5d47af8e72419351cc1a69092b7f
Valdemar Segerström <valdemar.segerstrom.java25@edu.edugrade.se>
2026-01-20 20:15:01 +0100

    Hmmm...

---
+++ 4Design&Arkitektur/Lektion1/Chapter1/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.example.chapter01.Example01_CodeSmellToPattern;
+
+public class Main {
+    public static void main(String[] args) {
+Example01_CodeSmellToPattern example01_codeSmellToPattern = new Example01_CodeSmellToPattern();
+
+        goodCalculator.setStrategy(new SameDayDelivery());
+        System.out.println("Same-day shipping cost: " + goodCalculator.calculate(weight) + " kr");
+
+    }
+}
